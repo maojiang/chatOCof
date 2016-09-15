cb01b17cdfc8acff87458596653a390137c7bfcf
maojiang <568213372@qq.com>
2016-09-15 12:38:25 +0800

    first commit

---
+++ xmppChatTests/xmppChatTests.m
@@ -0,0 +1,39 @@
+//
+//  xmppChatTests.m
+//  xmppChatTests
+//
+//  Created by 邓茂江 on 16/9/11.
+//  Copyright © 2016年 maojiang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface xmppChatTests : XCTestCase
+
+@end
+
+@implementation xmppChatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
